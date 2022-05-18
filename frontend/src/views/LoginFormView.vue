bdf61db5dc49ef3e76a3e3497d449e996ae94135
Herve Thi <thi.h3rve@gmail.com>
2022-05-18 18:40:29 +0200

    AJout d'une fonctionnalité de redirection si l'utilisateur n'est pas loggé

--- frontend/src/views/LoginFormView.vue
+++ frontend/src/views/LoginFormView.vue
@@ -1,15 +1,25 @@
 <template>
-    <LoginForm msg="Veuillez vous connecter" />
+  <div id="login">
+    <div class="title">
+      <img alt="Groupomania logo" src="../assets/Groupomania-logo-red.png" />
+      <h1>Réseau social interne</h1>
+    </div>
+    <LoginForm />
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { userLogged } from "@/functions/fetchUser.js"
 import LoginForm from "@/components/LoginForm.vue";
 
 export default {
   name: "LoginFormView",
   components: {
     LoginForm,
+  },
+  beforeCreate() {
+    if (userLogged()) this.$router.push("/whatsnew");
   },
 };
 </script>
